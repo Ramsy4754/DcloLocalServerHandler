25e21274345549dbdaa24379b928336809cfaeca
ramsy4754 <ramsy4754@clo-guard.com>
2024-02-26 17:40:43 +0900

    serverCmd Process Check 추가

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var (
 )
 
 func main() {
-	logFile, err := os.OpenFile("D:\\03_Private\\Go_Local_Server\\server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logFile, err := os.OpenFile("D:/03_Private/Go_Local_Server/server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Printf("error opening log file: %v\n", err)
 		os.Exit(1)
@@ -39,7 +39,7 @@ func startServerHandler(w http.ResponseWriter, r *http.Request) {
 	serverCmdMutex.Lock()
 	defer serverCmdMutex.Unlock()
 
-	if serverCmd != nil {
+	if serverCmd != nil && serverCmd.Process != nil {
 		_, _ = fmt.Fprintf(w, "Server is already running.\n")
 		return
 	}
@@ -49,7 +49,7 @@ func startServerHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	serverCmd = exec.Command("D:\\00_Development\\Dclo_Back\\venv\\Scripts\\python.exe", "D:\\00_Development\\Dclo_Back\\run.py")
+	serverCmd = exec.Command("D:/00_Development/Dclo_Back/venv/Scripts/python.exe", "D:/00_Development/Dclo_Back/run.py")
 	serverCmd.Env = append(os.Environ(), envVars...)
 	serverCmd.Env = append(serverCmd.Env, "SYSTEM_TYPE=LOCAL")
 

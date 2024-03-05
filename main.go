dcea43b80e51f7d9750d95da7511b65c4c70fde7
ramsy4754 <ramsy4754@clo-guard.com>
2024-03-05 09:11:10 +0900

    Admin Server Handler 추가

--- main.go
+++ main.go
@@ -12,8 +12,13 @@ import (
 )
 
 var (
-	serverCmd      *exec.Cmd
-	serverCmdMutex sync.Mutex
+	dcloServerCmd      *exec.Cmd
+	dcloServerCmdMutex sync.Mutex
+)
+
+var (
+	adminServerCmd      *exec.Cmd
+	adminServerCmdMutex sync.Mutex
 )
 
 func main() {
@@ -27,19 +32,60 @@ func main() {
 	// Set the output of the log package to the log file
 	log.SetOutput(logFile)
 
-	http.HandleFunc("/start", startServerHandler)
-	http.HandleFunc("/stop", stopServerHandler)
+	http.HandleFunc("/start", startDcloServerHandler)
+	http.HandleFunc("/stop", stopDcloServerHandler)
+
+	http.HandleFunc("/start-admin", startAdminServerHandler)
+	http.HandleFunc("/stop-admin", stopAdminServerHandler)
 
 	log.Println("Starting API server on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func startServerHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("startServerHandler", r.RemoteAddr)
-	serverCmdMutex.Lock()
-	defer serverCmdMutex.Unlock()
+func startAdminServerHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("startAdminServerHandler", r.RemoteAddr)
+	adminServerCmdMutex.Lock()
+	defer adminServerCmdMutex.Unlock()
 
-	if serverCmd != nil && serverCmd.Process != nil {
+	if adminServerCmd != nil && adminServerCmd.Process != nil {
+		_, _ = fmt.Fprintf(w, "Server is already running.\n")
+		return
+	}
+
+	dcloServerCmd = exec.Command("D:/00_Development/Dclo_Admin/.venv/Scripts/python.exe", "D:/00_Development/Dclo_Admin/run.py")
+	err := dcloServerCmd.Start()
+	if err != nil {
+		http.Error(w, "Failed to run python server", http.StatusInternalServerError)
+		return
+	}
+
+	_, _ = fmt.Fprintf(w, "Starting server...")
+}
+
+func stopAdminServerHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("stopAdminServerHandler", r.RemoteAddr)
+	adminServerCmdMutex.Lock()
+	defer adminServerCmdMutex.Unlock()
+
+	if adminServerCmd != nil && adminServerCmd.Process != nil {
+		err := adminServerCmd.Process.Kill()
+		if err != nil {
+			http.Error(w, "Failed to stop server", http.StatusInternalServerError)
+			return
+		}
+		adminServerCmd = nil
+		_, _ = fmt.Fprintf(w, "Server stopped successfully.")
+	} else {
+		_, _ = fmt.Fprintf(w, "Server is not running.")
+	}
+}
+
+func startDcloServerHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("startDcloServerHandler", r.RemoteAddr)
+	dcloServerCmdMutex.Lock()
+	defer dcloServerCmdMutex.Unlock()
+
+	if dcloServerCmd != nil && dcloServerCmd.Process != nil {
 		_, _ = fmt.Fprintf(w, "Server is already running.\n")
 		return
 	}
@@ -49,9 +95,9 @@ func startServerHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	serverCmd = exec.Command("D:/00_Development/Dclo_Back/venv/Scripts/python.exe", "D:/00_Development/Dclo_Back/run.py")
-	serverCmd.Env = append(os.Environ(), envVars...)
-	serverCmd.Env = append(serverCmd.Env, "SYSTEM_TYPE=LOCAL")
+	dcloServerCmd = exec.Command("D:/00_Development/Dclo_Back/venv/Scripts/python.exe", "D:/00_Development/Dclo_Back/run.py")
+	dcloServerCmd.Env = append(os.Environ(), envVars...)
+	dcloServerCmd.Env = append(dcloServerCmd.Env, "SYSTEM_TYPE=LOCAL")
 
 	// Run redis server
 	cmd := exec.Command("wsl", "redis-server")
@@ -61,7 +107,7 @@ func startServerHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = serverCmd.Start()
+	err = dcloServerCmd.Start()
 	if err != nil {
 		http.Error(w, "Failed to run python server", http.StatusInternalServerError)
 		return
@@ -98,18 +144,18 @@ func readEnvFile(filePath string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
-func stopServerHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("stopServerHandler", r.RemoteAddr)
-	serverCmdMutex.Lock()
-	defer serverCmdMutex.Unlock()
+func stopDcloServerHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("stopDcloServerHandler", r.RemoteAddr)
+	dcloServerCmdMutex.Lock()
+	defer dcloServerCmdMutex.Unlock()
 
-	if serverCmd != nil && serverCmd.Process != nil {
-		err := serverCmd.Process.Kill()
+	if dcloServerCmd != nil && dcloServerCmd.Process != nil {
+		err := dcloServerCmd.Process.Kill()
 		if err != nil {
 			http.Error(w, "Failed to stop server", http.StatusInternalServerError)
 			return
 		}
-		serverCmd = nil
+		dcloServerCmd = nil
 		fmt.Fprintf(w, "Server stopped successfully.")
 	} else {
 		fmt.Fprintf(w, "Server is not running.")
